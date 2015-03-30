4693ed880c4b9862a5f1ff1fa18d61bd39344779
Ajinkya Borade <ajinkyab1987@gmail.com>
2015-03-30 11:53:45 +0530

    grunt

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -21,14 +21,13 @@ module.exports = function(config) {
       // bower:js
       'bower_components/jquery/dist/jquery.js',
       'bower_components/angular/angular.js',
-      'bower_components/angular-mocks/angular-mocks.js',
       'bower_components/bootstrap/dist/js/bootstrap.js',
       'bower_components/angular-cookies/angular-cookies.js',
       'bower_components/angular-resource/angular-resource.js',
       'bower_components/angular-route/angular-route.js',
       'bower_components/angular-sanitize/angular-sanitize.js',
       'bower_components/angular-ui-router/release/angular-ui-router.js',
-      
+      'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       'app/scripts/**/*.js',
       'test/mock/**/*.js',
